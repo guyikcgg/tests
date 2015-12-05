59b4afdeb0c2dec1be9f6656ab2de253271973f9
Cristian G Guerrero <guyik.cgg@gmail.com>
2015-12-06 00:57:48 +0100

    function1 solving issue1 =)

--- funcion1.c
+++ funcion1.c
@@ -1,4 +1,18 @@
 //This will be the function that solves the issue 1,
 //but it is still in a very primitive status, so it will need a lot of modifications... I don't know what I can write to enlarge this line... I am testing Vim (and liking it) I want to use Vim!!
 
+/* Well, this is an advance in this function.
+ * Let's say it will solve the issue 1.
+ */
 
+/*******************************
+ * Another test to Vim, I am   *
+ * liking it know. But... will *
+ * it be so powerful?          *
+ *******************************/
+
+int function1() {
+	wow! it has automatic indentation!!;
+
+	return 0;
+}
