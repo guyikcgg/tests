a0b08b3998bfcaab8832e182feea21dd458c9f5c
Cristian G Guerrero <guyik.cgg@gmail.com>
2015-12-06 00:54:23 +0100

    Added function1.c
    It will probably solve the issue1

---
+++ funcion1.c
@@ -0,0 +1,4 @@
+//This will be the function that solves the issue 1,
+//but it is still in a very primitive status, so it will need a lot of modifications... I don't know what I can write to enlarge this line... I am testing Vim (and liking it) I want to use Vim!!
+
+
