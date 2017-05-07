c1355a2026ee76fceaf21a81f8034db2ddb7e13f
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-05-08 00:56:19 +0200

    Add some comments

--- bashtests/iterate_url.sh
+++ bashtests/iterate_url.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Print Usage and exit
 usage() {
     echo "Usage: $0 [-c <command>] <url.list>" 1>&2
     echo "" 1>&2
@@ -8,17 +9,16 @@ usage() {
     exit 1
 }
 
+# URL variables
 MY_URL_1="http://www."
 MY_URL_2=".com/"
 
+# Default command
 command=echo
 
+# Get the options
 while getopts ":s:c:" o; do
     case "${o}" in
-        s)
-            s=${OPTARG}
-            ((s == 45 || s == 90)) || usage
-            ;;
         c)
             command=${OPTARG}
             ;;
@@ -30,16 +30,18 @@ done
 shift $((OPTIND-1))
 
 
-
+# Check that a file was specified
 if [ -z "$1" ]; then
     usage
 fi
 
+# Check that the file exists
 if [ ! -f "$1" ]; then
     echo "$0: $1: file not found" 1>&2
     exit 1
 fi
 
+# Iterate over every line in the file
 for i in $( cat $1 ); do
     $command "$MY_URL_1""$i""$MY_URL_2"
 done
